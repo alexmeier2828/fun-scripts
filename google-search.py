6fb44576be9e5a1a426ce1c2f5a13dd8467590ff
Alex Meier <alexmeier@Alexs-MacBook-Pro.local>
2020-02-20 10:44:43 -0500

    opens w3m

--- google-search.py
+++ google-search.py
@@ -3,9 +3,11 @@ try:
 except ImportError:
     print("missing dep")
 import sys
+import subprocess 
 
 #args
 query = sys.argv[0]
+
 
 
 results = search(query, tld='com', lang='en', num=10, start=0, stop=10, pause=1.0)
@@ -13,3 +15,5 @@ results = search(query, tld='com', lang='en', num=10, start=0, stop=10, pause=1.
 
 for r in results:
     print(r)
+    p = subprocess.Popen(["w3m",str(r)]).wait()  
+
